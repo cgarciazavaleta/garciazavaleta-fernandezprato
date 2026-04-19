f49d5aef94e25dcbb217e31aa76aa8ffc535b628
cgarciazavaleta <cgarciazavaleta@udesa.edu.ar>
2026-04-19 20:08:40 -0300

    log in y register

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from "react-router-dom/cjs/react-router-dom.min"
-function Navbar(props) {
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
-    let usuarioLogueado = false;
-  
-  return (
-    <header>
-      <div className="logo"><img src="./imagenes/Logo.png" alt="logo" /></div>
-      <nav>
-        <ul className="nav">
-           {
-                props.menu.map((elementoMenu,idx)=><li key={elementoMenu + idx}> <Link to={elementoMenu.Path}>{elementoMenu.Nombre}</Link> </li>)
-            }
-        </ul>
-      </nav>
-    </header>
-  );
+class Navbar extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            usuarioLogueado: cookies.get("usuarioCookies")
+        }
+    }
+
+    componentDidUpdate() {
+        if (this.state.cookie !== cookies.get("usuarioCookies")) {
+            this.setState({
+                usuarioLogueado: cookies.get("usuarioCookies")
+            });
+        }
+    }
+
+    cerrarSesion() {
+        cookies.remove("usuarioCookies");
+        this.setState({
+            usuarioLogueado: cookies.get("usuarioCookies")})
+    }
+    render(){
+      let usuarioLogueado = cookies.get("usuarioCookies");
+      return (
+          <header>
+            <div className="logo"><img src="./imagenes/Logo.png" alt="logo" /></div>
+            <nav>
+              <ul className="nav">
+                {
+                  this.props.menu.map((elementoMenu,idx)=>
+                  <li key={elementoMenu + idx} className={elementoMenu.Nombre === "Iniciarse"? (this.state.usuarioLogueado == null ? "show" : "hide"):
+                    elementoMenu.Nombre === "Registrarse" ? (this.state.usuarioLogueado == null ? "show" : "hide"):
+                    elementoMenu.Nombre === "Favoritos" ? (this.state.usuarioLogueado != null ? "show" : "hide"):
+                    "show"}> 
+                    <Link to={elementoMenu.Path}>{elementoMenu.Nombre}</Link> 
+                  </li>)
+                }
+                {
+                  this.state.usuarioLogueado != null ?
+                    <li>
+                      <button onClick={() => this.cerrarSesion()}>
+                          Cerrar sesión
+                      </button>
+                    </li>
+                    : null
+                }
+              </ul>
+            </nav>
+          </header>
+      )
+    }
+
 }
+
 export default Navbar;
